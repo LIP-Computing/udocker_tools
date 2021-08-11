8add79915903d4fb3e4d736fae3e285faaa64e18
mariojmdavid <mariojmdavid@gmail.com>
2021-08-11 17:14:21 +0100

    script to produce the metadata.json

--- utils/make-meta.py
+++ utils/make-meta.py
@@ -12,7 +12,44 @@
 import os
 import json
 import csv
+import pprint
+
+def create_jsonmodule():
+    mod_json = {"module": None,
+                "fname": None,
+                "version": None,
+                "arch": None,
+                "os": None,
+                "os_ver": None,
+                "kernel_ver": None,
+                "dependencies": list(),
+                "sha256sum": None,
+                "urls": list(),
+                "docs_url": list()}
+    return mod_json
 
 if __name__ == '__main__':
     fjson = "../metadata.json"
-    fcsv = "../flist.csv"
+    fcsv = "../flist-keep.csv"
+    all_mod = list()
+    url_dirs = ["https://download.ncg.ingrid.pt/webdav/udocker/engines/tarballs/",
+               "https://github.com/LIP-Computing/udocker_tools/raw/main/tarballs/"]
+
+    with open(fcsv, 'r') as csvin:
+        csv_reader = csv.DictReader(csvin, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+
+            mod_json = create_jsonmodule()
+            for key in row:
+                mod_json[key] = row[key]
+
+            for urldir in url_dirs:
+                mod_json["urls"].append(urldir + row["fname"])
+
+            line_count += 1
+            all_mod.append(mod_json)
+
+    pprint.pprint(all_mod)
