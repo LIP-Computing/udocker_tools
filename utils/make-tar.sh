4324324c1561bf8f2a41cd55ab8f15577743967f
mariojmdavid <mariojmdavid@gmail.com>
2023-03-24 15:40:47 +0000

    updated improved script make tarballs

--- utils/make-tar.sh
+++ utils/make-tar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2021 LIP
+# Copyright 2021-2023 LIP
 #
 # Author: Mario David <mariojmdavid@gmail.com>
 #
@@ -8,20 +8,45 @@
 
 cd ..
 DIR=`pwd`
-BIN="${DIR}/engines/bin"
-LIB="${DIR}/engines/lib"
 TAR_DIR="${DIR}/tarballs"
 FILE_LIST=${DIR}/flist.csv
+TMPDIR=${HOME}/tmpdir
+mkdir -p ${TMPDIR}
 
-for direxe in ${BIN} ${LIB}
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <udocker tools tarball version>"
+    echo "Example: $0 1.2.9"
+    exit 1
+fi
+
+echo "wget udocker tools tarball to ${TMPDIR}"
+cd ${TMPDIR}
+wget --no-check-certificate https://download.ncg.ingrid.pt/webdav/udocker/udocker-englib-${1}.tar.gz
+
+echo "unzip/untar udocker-englib-${1}.tar.gz"
+tar zxvf udocker-englib-${1}.tar.gz
+
+BIN="${TMPDIR}/udocker_dir/bin"
+LIB="${TMPDIR}/udocker_dir/lib"
+
+rm ${FILE_LIST}
+cd ${BIN}
+for fname in `find * -type f`
 do
-    cd ${direxe}
-    for fname in `find * -type f`
-    do
-        TAR="${TAR_DIR}/${fname}.tgz"
-        tar zcvf ${TAR} ${fname}
-        line=`sha256sum ${TAR}`
-        SHA256=$(echo $line | awk -F' ' '{printf "%s", $1}' | tr -d '"')
-        echo "${fname}.tgz,${SHA256}" >> ${FILE_LIST}
-    done
+    TAR="${TAR_DIR}/${fname}.tgz"
+    tar zcvf ${TAR} ${fname}
+    line=`sha256sum ${TAR}`
+    SHA256=$(echo $line | awk -F' ' '{printf "%s", $1}' | tr -d '"')
+    echo "${fname}.tgz,${SHA256}" >> ${FILE_LIST}
+done
+
+cd ${LIB}
+for fname in `find * -name 'libfakechroot-*' -type f`
+do
+    TAR="${TAR_DIR}/${fname}.tgz"
+    tar zcvf ${TAR} ${fname}
+    line=`sha256sum ${TAR}`
+    SHA256=$(echo $line | awk -F' ' '{printf "%s", $1}' | tr -d '"')
+    echo "${fname}.tgz,${SHA256}" >> ${FILE_LIST}
 done
